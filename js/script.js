3059bcf1b10027d67ad921e20818a294f67d1801
thiago-neves <37526439+thiago-neves@users.noreply.github.com>
2018-07-29 04:53:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,17 +13,14 @@ $(function(){
 
 function cor(){
   var NLetras = document.getElementById("txtNome").value;
-
-if(NLetras.length < 6){
-  document.getElementById("txtNome").className = "uk-form-danger uk-input";
-};
-
-if(NLetras.length > 6 ){
-  document.getElementById("txtNome").className = "uk-form-success uk-input";
-};
+  
 if(NLetras == ""){
   document.getElementById("txtNome").className = "form-golden uk-input";
-}
+} else if(NLetras.length <= 2){
+  document.getElementById("txtNome").className = "uk-form-danger uk-input";
+} else {
+  document.getElementById("txtNome").className = "uk-form-success uk-input";
+};
   
 }
 
